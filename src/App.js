8f33f657f3439c59b69433cf8f24a9e2c1e647cf
n1cnacs <68463382+n1cnacs@users.noreply.github.com>
2020-11-30 19:11:03 +0000

    Added Open source text, included classes to test Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <header className="App-header">
         <h1>
         Weather App        
         </h1>
+        <div className="Card App-body">
+      <div className="Card-body">
         < Weather />
+        </div>
+        </div>
+      <h3>Open Source coded by Nicola Warner</h3>
+        
         </header>
     </div>
   );
